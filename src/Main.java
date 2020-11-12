d555b9ea3b4d6a073063b8965121c455530cc66e
FilipePpereira <asw123asw123asw>
2020-11-12 10:45:46 +0000

    add input and output

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,18 @@
  *
  */
 
+import java.util.Scanner;
+
 import static java.lang.System.*;
 
 public class Main
 {
     public static void main(String[] args)
     {
-
+        String name = null;
+        Scanner keyboard = new Scanner(System.in);
+        out.println("Enter your name");
+        name = keyboard.nextLine();
+        out.printf("your name: %s" , name);
     }
 }
