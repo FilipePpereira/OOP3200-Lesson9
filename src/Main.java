4b756c4b4838b0a9352ac1265274294f725c25b2
FilipePpereira <asw123asw123asw>
2020-11-12 10:57:26 +0000

    add input and output

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,20 @@ public class Main
         out.println("Enter your name");
         name = keyboard.nextLine();
         out.printf("your name: %s" , name);
+        keyboard.nextLine();
+
+        float age = 0.0f;
+        out.println("Enter your Age");
+        age = keyboard.nextFloat();
+        keyboard.nextLine();
+        out.printf("your name: %s" , age);
+        keyboard.nextLine();
+
+        int idStudent = 0;
+        out.println("Enter your ID the Student");
+        idStudent = keyboard.nextInt();
+        keyboard.nextLine();
+        out.printf("your name: %s" , idStudent);
+
     }
 }
